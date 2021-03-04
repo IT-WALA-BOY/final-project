662711395df5eac750413e703d5b4c93d6ceda54
IT-WALA-BOY <bsitbeve@gmail.com>
2021-03-04 11:14:43 +0500

    Murshad

---
+++ ahmad tpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int math,eng,comp;
+	int sum;
+	
+	cout<<"enter marks of math: "<<math<<endl;
+	cin>>math;
+	cout<<"enter marks of eng"<<eng<<endl;
+	cin>>eng;
+	cout<<"enter maeks of comp: "<<comp<<endl;
+	cin>>comp;
+	sum=math+eng+comp;
+	cout<<"disply sum of numbers : "<<sum<<endl;
+	
+}
