a73b888fc957977cd24b8e18f8491f61e644c32f
IT-WALA-BOY <bsitbeve@gmail.com>
2021-03-04 11:11:06 +0500

    daily upload

---
+++ fpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	int sum;
+	
+	cout<<"enter marks of math: "<<a<<endl;
+	cin>>a;
+	cout<<"enter marks of eng"<<b<<endl;
+	cin>>b;
+	cout<<"enter maeks of comp: "<<c<<endl;
+	cin>>c;
+	sum=a+b+c;
+	cout<<"disply sum of numbers : "<<sum<<endl;
+	
+}
