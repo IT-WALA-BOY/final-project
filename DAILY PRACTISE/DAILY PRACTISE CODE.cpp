c2ee72a3279087584fdcb1423e98cffff4381055
IT-WALA-BOY <bsitbeve@gmail.com>
2021-03-09 17:52:45 +0500

    Daily upload 1

---
+++ DAILY PRACTISE/DAILY PRACTISE CODE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int p , q;
+	cin>>p;
+	cin>>q;
+	if (p<q)
+	{
+		cout<<"P is a prime number. ";
+	}
+	else
+	{
+		cout<<"P is Rational recuring number. ";
+	}
+}
